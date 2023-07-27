798fe1d17fca0178b7247c4f7d123b088a8a10f1
[Ghias Naser] <[ghiasnaser@gmail.com]>
2023-07-26 23:05:52 -0700

    create the thought  schema

---
+++ models/Thought.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const reactionSchema = require('./Reaction'); // Import the reactionSchema
+
+const thoughtSchema = new mongoose.Schema({
+  thoughtText: {
+    type: String,
+    required: true,
+    minlength: 1,
+    maxlength: 280,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: (timestamp) => dateFormat(timestamp), // Implement the dateFormat function
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  reactions: [reactionSchema], // Using the imported Reaction subdocument schema as an array of nested documents
+});
+
+// Virtual to get the length of the thought's reactions array field
+thoughtSchema.virtual('reactionCount').get(function () {
+  return this.reactions.length;
+});
+
+const Thought = mongoose.model('Thought', thoughtSchema);
+
+module.exports = Thought;
